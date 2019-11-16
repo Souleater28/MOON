9e894aac7bd10f1cc4eeab78f5f55e5c06715a27
Souleater28 <55631076+Souleater28@users.noreply.github.com>
2019-11-16 15:17:00 +0200

    Add files via upload

--- Calc/Calc.js
+++ Calc/Calc.js
@@ -1,25 +1,80 @@
 let calcInput = document.getElementById("vvod");
-let inpValue;
-function val() {
-  inpValue = calcInput.value;
-  console.log(inpValue);
+const regExp = /[\d,+,\/,\-,*,.]/;
+const forbidden = [
+  18,
+  112,
+  113,
+  114,
+  115,
+  116,
+  117,
+  118,
+  119,
+  120,
+  121,
+  122,
+  123
+];
+function proverka(event) {
+  console.log("INDEX OF", forbidden.indexOf(event.keyCode));
+
+  console.log(regExp.test(event.key));
+  event.preventDefault();
+
+  switch (event.keyCode) {
+    case 13:
+    case 187:
+      calc(event);
+      break;
+    case 8:
+      calcInput.value = calcInput.value.slice(0, calcInput.value.length - 1);
+      break;
+    case 46:
+      calcInput.value = " ";
+      break;
+  }
+  switch (event.key) {
+    case "s":
+      calcInput += "sin(";
+    case "c":
+      calcInput += "cos(";
+    case "t":
+      calcInput += "tan(";
+    case "C":
+      calcInput += "ctg(";
+  }
+  if (
+    !forbidden.indexOf(event.keyCode) ||
+    forbidden.indexOf(event.keyCode) === -1
+  ) {
+    if (regExp.test(event.key)) {
+      putValue(event.key);
+    }
+  }
 }
-// function proverka(input) {
-//   var regExp = /[0-9]/;
-//   console.log(regExp.test(calcInput.value));
-// }
-function vivod(inpValue) {
-  calcInput.innerHTML += inpValue;
-}
+
 function calc(event) {
-  inpValue = eval(inpValue);
-  vivod(inpValue);
+  calcInput.value = eval(calcInput.value);
 }
-function number(event) {}
+
+function putValue(value) {
+  switch (value) {
+    case "C":
+      calcInput.value = "";
+      break;
+    case "=":
+      calc(event);
+      break;
+
+    default:
+      calcInput.value += value;
+      break;
+  }
+}
 
 const buttons = Array.from(document.getElementsByClassName("button"));
-buttons.map((item, index) => {
-  if (!item.classList.contains("_")) {
-    item.addEventListener("click", () => num(event));
+buttons.map((button, index) => {
+  if (!button.classList.contains("_")) {
+    button.addEventListener("click", () => putValue(event.target.id));
   }
 });
